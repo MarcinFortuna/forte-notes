e31e7413b412a0277b0ad7cbb3933c913f66f540
Marcin Fortuna <marcin.fort@gmail.com>
2025-07-13 16:01:33 +0200

    Create header and its content

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,33 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import {shadow} from "@/styles/utils";
+import {Button} from "@/components/ui/button";
+import DarkModeToggle from "@/components/DarkModeToggle";
 
 export default function Header() {
-    return <header>
-        <Link href="/">
+
+    const user = null;
+
+    return <header className="relative w-full flex h-24 items-center justify-between bg-popover px-3 sm:px-8" style={{
+        boxShadow: shadow
+    }}>
+        <Link className="flex items-end gap-2" href="/">
             <Image src="/ForteNotesLOGO.png" alt="Forte Notes" height={60} width={60} className="rounded" priority/>
+            <h1 className="flex flex-col pb-1 text-2xl font-semibold leading-6">Forte <span>Notes</span></h1>
         </Link>
+        <div className="flex gap-4">
+            {
+                user ? "Logout" : <>
+                    <Button asChild>
+                        <Link href="/sign-up" className="hidden sm:block">Sign Up</Link>
+                    </Button>
+                    <Button asChild className="outline">
+                        <Link href="/login">Login</Link>
+                    </Button>
+                </>
+            }
+            <DarkModeToggle />
+        </div>
     </header>
 }
